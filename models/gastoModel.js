31e93a4e792660dbbbd16353fc83b0df25470483
Norberto Caceres <norberto@clau.io>
2019-09-28 20:32:44 -0500

    expandir el modelo Gasto  para que incluya tipoDePrograma y programa

--- models/gastoModel.js
+++ models/gastoModel.js
@@ -39,6 +39,48 @@ const gastoSchema = new mongoose.Schema({
       message: 'El monto del gasto debe ser mayor a cero',
     },
   },
+  tipoDePrograma: {
+    type: String,
+    required: [true, 'Es requerido especificar el tipo de programa'],
+    enum: {
+      values: ['Funcionamiento', 'Inversión'],
+      message: 'Tipo inválido de programa',
+    },
+  },
+  programa: {
+    type: String,
+    required: [true, 'Es requerido especificar el programa al cúal pertenece el gasto'],
+    validate: {
+      validator: function(value) {
+        const programasFuncionamiento = [
+          'dirección-y-administración-general',
+          'educación-básica-general',
+          'educación-media',
+          'educación-de-adultos',
+          'educación-post-media',
+          'transferencias-corrientes',
+          'transferencias-al-sector-público',
+          'fondo-de-exoneración-de-matrícula',
+          'educación-agropecuaria',
+        ]
+        const programasInversion = [
+          'mantenimiento-preventivo',
+          'construcción-y-rehabilitación-de-escuelas',
+          'equipamiento-de-centros-educativos',
+          'desarrollo-educativo',
+          'nutrición-escolar',
+          'educación-básica',
+          'transferencias-de-capital',
+          'fortalecimiento-de-tecnología-educativa',
+          'mantenimiento-equipo-construcción',
+        ]
+
+        if (this.tipoDePrograma === 'Funcionamiento') return programasFuncionamiento.includes(value)
+        // si llegamos a esta línea es porque es de tipo 'Inversión'
+        return programasInversion.includes(value)
+      },
+    },
+  },
   destinoTipo: {
     type: String,
     required: [true, 'Es requerido especificar el tipo de destino del gasto'],
