da5f33f9af82665e84d1e7de030d5a7bfcb3905c
Norberto Caceres <norberto@clau.io>
2019-09-28 19:37:24 -0500

    agregar funciones de instancia de utilidad para auth

--- models/systemUserModel.js
+++ models/systemUserModel.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const systemUserSchema = new mongoose.Schema({
   cedula: {
@@ -23,6 +25,24 @@ const systemUserSchema = new mongoose.Schema({
       'Es necesario especificar la entidad a la cual pertenece el usuario del sistema',
     ],
   },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'user must confirm their password'],
+    validate: {
+      validator: function(value) {
+        return value === this.password
+      },
+      message: 'password and password confirmation do not match',
+    },
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetTokenExpiryDate: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 })
 
 systemUserSchema.virtual('gastos', {
@@ -36,6 +56,56 @@ systemUserSchema.virtual('ingresos', {
   foreignField: 'user',
   localField: '_id',
 })
+
+systemUserSchema.pre('save', async function(next) {
+  // only run if the password was modified. create() returns true for this.isModified for all fields, so the middleware WILL run on create(), which is what we want
+  if (!this.isModified('password')) return next()
+
+  // hash with cost of 12
+  this.password = await bcrypt.hash(this.password, 12)
+  this.passwordConfirm = undefined
+  next()
+})
+
+systemUserSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next()
+
+  this.passwordChangedAt = Date.now() - 1000
+  next()
+})
+
+systemUserSchema.pre(/^find/, function(next) {
+  this.find({ active: { $ne: false } })
+  next()
+})
+
+systemUserSchema.methods.validatePassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword)
+}
+
+systemUserSchema.methods.changedPasswordAfterJwt = function(JwtTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimeStamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10)
+
+    return JwtTimestamp < changedTimeStamp
+  }
+
+  // password not changed
+  return false
+}
+
+systemUserSchema.methods.generatePasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex')
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex')
+
+  this.passwordResetTokenExpiryDate = Date.now() + 600000
+
+  return resetToken
+}
 
 const SystemUser = mongoose.model('SystemUser', systemUserSchema)
 
